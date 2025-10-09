e43ed1a9737503cd6a214ce866d4cf0c7bc5194a
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-10-10 07:10:37 +0900

    [Update]'dbapp.py'を書き換え

--- dbapp/dbapp.py
+++ dbapp/dbapp.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import pyodbc
+import db.queries as db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("pages/index.html")
+    # DBに接続してレコードセットを取得
+    columns, rows = db.fetch_all(db.TEST_QUERY)
+
+    # レコードセットをテンプレートに渡す
+    return render_template(
+        "pages/index.html", 
+        columns=columns, 
+        rows=rows
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
