b57b6e36d8566f2bca714257c2d614bf3f25e7e5
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-10-11 06:31:30 +0900

    [Add]'dbapp.py'にテーブル構造表示ページのルートを追加

--- dbapp/dbapp.py
+++ dbapp/dbapp.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 import pyodbc
 import db.queries as db
 
 app = Flask(__name__)
 
+# トップページ
 @app.route("/")
 def index():
     # DBに接続してレコードセットを取得
@@ -12,6 +13,30 @@ def index():
     # レコードセットをテンプレートに渡す
     return render_template(
         "pages/index.html", 
+        columns=columns, 
+        rows=rows
+    )
+
+# 各テーブルの構造表示用ページ
+@app.route("/table/<table_name>")
+def show_table_structure(table_name):
+    # 表示するテーブル名のリスト
+    allowed_tables = [
+        "BelongTo", "Categories", "CustomerClasses", "Customers", 
+        "Departments", "Employees", "Prefecturals", "Products", 
+        "Salary", "Sales"
+    ]
+    # テーブル名がリストになかったら404
+    if table_name not in allowed_tables:
+        abort(404)
+    
+    # DESC文実行
+    columns, rows = db.describe_table(table_name)
+
+    # テンプレートにデータを投げる
+    return render_template(
+        "pages/table.html", 
+        table_name=table_name, 
         columns=columns, 
         rows=rows
     )
