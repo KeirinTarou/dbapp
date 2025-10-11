717924e884a3d06682ad168607e336528525c666
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-10-11 09:13:10 +0900

    [Add]'dbapp.py': クエリ投稿->レコードセット表示ロジックを追加

--- dbapp/dbapp.py
+++ dbapp/dbapp.py
@@ -1,21 +1,48 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
 import pyodbc
 import db.queries as db
 
 app = Flask(__name__)
 
-# トップページ
-@app.route("/")
-def index():
-    # DBに接続してレコードセットを取得
-    columns, rows = db.fetch_all(db.TEST_QUERY)
+# クエリ実行失敗時返却用データ（笑）
+FAILED_COLUMNS = ["( ´,_ゝ`)", "ち～ん（笑）"]
+FAILED_ROWS = [
+    ["残念ｗ", "レコードセットが返らなかったよｗｗｗ"]
+]
 
+DEFAULT_COLUMNS = ["( ´_ゝ`)", "クエリ未実行"]
+DEFAULT_ROWS = [
+    ["(･ω･)", "クエリを入力して実行ボタンをクリックしてね！"]
+]
+
+# トップページ
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # POSTリクエストのとき
+    if request.method == "POST":
+        # フォームからクエリを取得
+        sql_query = request.form.get("sql_query", "").strip()
+        # クエリ実行 -> レコードセット取得
+        try:
+            columns, rows = db.fetch_all(sql_query)
+        except Exception as e:
+            columns = FAILED_COLUMNS
+            rows = FAILED_ROWS.copy()
+            # エラー情報を追加
+            rows.append(["原因はたぶん……", str(e)[:80] + "..."])
+    # GETリクエストのとき
+    else:
+        sql_query = ""
+        # デフォルトの擬似テーブルを表示
+        columns, rows = [DEFAULT_COLUMNS, DEFAULT_ROWS]
+    print(columns, rows)
     # レコードセットをテンプレートに渡す
     return render_template(
         "pages/index.html", 
         columns=columns, 
         rows=rows, 
-        table_names=db.TABLE_NAMES
+        table_names=db.TABLE_NAMES, 
+        sql_query=sql_query
     )
 
 # 各テーブルの構造表示用ページ
