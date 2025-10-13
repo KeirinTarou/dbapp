b5732e638c4a2c5f322e4a15b0305d855e559329
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-10-13 22:03:33 +0900

    [Fix]'main.js': parseInt()のタイプミスを修正

--- dbapp/static/js/main.js
+++ dbapp/static/js/main.js
@@ -30,9 +30,9 @@
         });
 
         // CodeMirrorラッパーのdata属性から受け取った値をCodeMirrorオブジェクトにセット
-        const height = $("#sql_query_wrapper").data('sql-query-height')
+        const height = $('#sql_query_wrapper').data('sql-query-height')
         if (height) {
-            editor.setSize("100%", parsInt(height) + "px");
+            editor.setSize("100%", parseInt(height) + "px");
         }
 
         // フォームサブミット時にCodeMirrorラッパーの高さをinput:hiddenに入れる
