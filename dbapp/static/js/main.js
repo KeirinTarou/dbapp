9d1cfd025e50ca73c375a34f450db721a87f3ca9
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-10-14 06:59:45 +0900

    [Add]'main.js': data-scroll属性がtrueのときにスクロールさせる処理を追加

--- dbapp/static/js/main.js
+++ dbapp/static/js/main.js
@@ -3,58 +3,75 @@
 {
 
     $(function() {
-        // textareaをCodeMirrorに置き換え
-        const editor = 
-            CodeMirror.fromTextArea(document.getElementById('sql_query'), {
-            mode: 'text/x-sql', 
-            theme: 'eclipse', 
-            lineNumbers: true, 
-            indentWithTabs: true, 
-            smartIndent: true, 
-            autofocus: true, 
-            tabSize: 4, 
-            indentUnit: 4, 
-            placeholder: "( ´_ゝ`) < SELECT文を入力してください。", 
-            extraKeys: {
-                "Tab": function(cm) {
-                    if (cm.somethingSelected()) {
-                        cm.indentSelection("add");
-                    } else {
-                        cm.replaceSelection("\t");
+        /* --------------------------------------------------------------------
+            CodeMirror関係
+        -------------------------------------------------------------------- */
+            // textareaをCodeMirrorに置き換え
+            const editor = 
+                CodeMirror.fromTextArea(document.getElementById('sql_query'), {
+                mode: 'text/x-sql', 
+                theme: 'eclipse', 
+                lineNumbers: true, 
+                indentWithTabs: true, 
+                smartIndent: true, 
+                autofocus: true, 
+                tabSize: 4, 
+                indentUnit: 4, 
+                placeholder: "( ´_ゝ`) < SELECT文を入力してください。", 
+                extraKeys: {
+                    "Tab": function(cm) {
+                        if (cm.somethingSelected()) {
+                            cm.indentSelection("add");
+                        } else {
+                            cm.replaceSelection("\t");
+                        }
+                    }, 
+                    "Shift-Tab": function(cm) {
+                        cm.indentSelection("subtract");
                     }
-                }, 
-                "Shift-Tab": function(cm) {
-                    cm.indentSelection("subtract");
                 }
+            });
+
+            // CodeMirrorラッパーのdata属性から受け取った値をCodeMirrorオブジェクトにセット
+            const height = $('#sql_query_wrapper').data('sql-query-height')
+            if (height) {
+                editor.setSize("100%", parseInt(height) + "px");
             }
-        });
 
-        // CodeMirrorラッパーのdata属性から受け取った値をCodeMirrorオブジェクトにセット
-        const height = $('#sql_query_wrapper').data('sql-query-height')
-        if (height) {
-            editor.setSize("100%", parseInt(height) + "px");
-        }
+            // フォームサブミット時にCodeMirrorラッパーの高さをinput:hiddenに入れる
+            $('form').on('submit', function() {
+                const height = $('#sql_query_wrapper').outerHeight();
+                $('#sql_query_height').val(height);
+            });
 
-        // フォームサブミット時にCodeMirrorラッパーの高さをinput:hiddenに入れる
-        $('form').on('submit', function() {
-            const height = $('#sql_query_wrapper').outerHeight();
-            $('#sql_query_height').val(height);
-        });
+            // フォームサブミット時にCodeMirrorの値をtextareaに反映
+            $('form').on('submit', function() {
+                editor.save(); // textareaの値にコピー
+            });
 
-        // フォームサブミット時にCodeMirrorの値をtextareaに反映
-        $('form').on('submit', function() {
-            editor.save(); // textareaの値にコピー
-        });
-
-        $("#sql_query_wrapper").resizable({
-            handles: "s", // 下側だけリサイズ可
-            minHeight: 200, 
-            maxHeight: 600, 
-            resize: function(event, ui) {
-                // CodeMirrorの高さをラッパーdivに合わせる
-                editor.setSize(null, ui.size.height);
+            $("#sql_query_wrapper").resizable({
+                handles: "s", // 下側だけリサイズ可
+                minHeight: 200, 
+                maxHeight: 600, 
+                resize: function(event, ui) {
+                    // CodeMirrorの高さをラッパーdivに合わせる
+                    editor.setSize(null, ui.size.height);
+                }
+            });
+        
+        /* --------------------------------------------------------------------
+            スクロール関係
+        -------------------------------------------------------------------- */
+            const $scrollTarget = $('section[data-scroll="true"]');
+            if ($scrollTarget.length) {
+                $('html, body').animate(
+                    {
+                        scrollTop: $scrollTarget.offset().top
+                    }, 
+                    600, 
+                    'swing' 
+                );
             }
-        });
     });
 
 }
