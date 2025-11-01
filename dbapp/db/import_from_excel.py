5e369a640d1496cf2fedc02874a9ef0a627269ec
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2025-11-01 17:54:31 +0900

    [Update]'db/import_from_excel.py': fetch_all_excel()内でCOMを初期化し、Excelを踏み台にしてDBからレコードセットを取得するよう改変

--- dbapp/db/import_from_excel.py
+++ dbapp/db/import_from_excel.py
@@ -22,46 +22,54 @@ def fetch_all_excel(query: str):
     """ Excel経由でDBからレコードセットを取得
         (columns, rows)の形で結果を返却
     """
-    excel = win32com.client.Dispatch("Excel.Application")
-    excel.Visible = False
-    wb = excel.Workbooks.Open(EXCEL_PATH)
+    # Flaskのスレッド内でCOMを初期化
+    pythoncom.CoInitialize()
 
     try:
-        # 踏み台Excelブックのマクロに引数を渡して実行
-        excel.Run(
-            "Std01Main.RefreshFromDB", 
-            CONNECTION_STRING.replace("\n", ""), 
-            query
-        )
+        excel = win32com.client.Dispatch("Excel.Application")
+        excel.Visible = False
+        wb = excel.Workbooks.Open(EXCEL_PATH)
 
-        # 完了待ち
-        while True:
-            status = wb.Sheets(STATUS_SHEET).Range(STATUS_CELL).Value 
-            if status == STATUS_DONE:
-                break
-            elif status == STATUS_ERROR:
-                raise RuntimeError("ExcelでのDB処理中にエラーが発生しました。")
-            time.sleep(0.5)
+        try:
+            # 踏み台Excelブックのマクロに引数を渡して実行
+            excel.Run(
+                "Std01Main.RefreshFromDB", 
+                CONNECTION_STRING.replace("\n", ""), 
+                query
+            )
 
-        # 結果取得
-        ws = wb.Sheets(DATA_SHEET)
-        data = ws.UsedRange.Value
+            # 完了待ち
+            while True:
+                status = wb.Sheets(STATUS_SHEET).Range(STATUS_CELL).Value 
+                if status == STATUS_DONE:
+                    break
+                elif status == STATUS_ERROR:
+                    raise RuntimeError("ExcelでのDB処理中にエラーが発生しました。")
+                time.sleep(0.5)
 
-        # data[0] -> カラム名の行
-        # data[1:] -> データの行
+            # 結果取得
+            ws = wb.Sheets(DATA_SHEET)
+            data = ws.UsedRange.Value
 
-        if not data or len(data) < 2:
-            # データがない
-            return [], []
+            # data[0] -> カラム名の行
+            # data[1:] -> データの行
 
-        # カラム名のリスト
-        columns = list(data[0])
-        # データ行のリスト（タプルのリスト）
-        rows = [tuple(r) for r in data[1:]]
+            if not data or len(data) < 2:
+                # データがない
+                return [], []
 
-        # 結果を返却
-        return columns, rows
+            # カラム名のリスト
+            columns = list(data[0])
+            # データ行のリスト（タプルのリスト）
+            rows = [tuple(r) for r in data[1:]]
 
+            # 結果を返却
+            return columns, rows
+
+        finally:
+            wb.Close(SaveChanges=False)
+            excel.Quit()
+    
     finally:
-        wb.Close(SaveChanges=False)
-        excel.Quit()
+        # あとかたづけ
+        pythoncom.CoUninitialize()
