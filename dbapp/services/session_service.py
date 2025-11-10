64cef2bc67fb560877c1c93186f27e0a3614f57b
seo tomohiro <seoo_japan_keirinoh@yahoo.co.jp>
2025-11-11 06:55:17 +0900

    [Add]'services/session_service.py': save_editor_query()、pop_editor_query()を追加（併せて、セッションキーにページ名を追加するよう改修）

--- dbapp/services/session_service.py
+++ dbapp/services/session_service.py
@@ -1,25 +1,35 @@
 from flask import session
 from dbapp.config import DEFAULT_EDITOR_HEIGHT
 
-def save_query_editor_height(sql_query_height):
+def save_editor_query(sql_query: str, page: str):
+    """ エディタのクエリをセッションに保存
+    """
+    session[f"{page}_last_posted_query"] = sql_query
+
+def pop_editor_query(page: str):
+    """ セッションに保存されたクエリを返す
+    """
+    return session.pop(f"{page}_last_posted_query", "")
+
+def save_query_editor_height(sql_query_height: str, page: str):
     """ 渡された値をセッションに保存
     """
     try:
-        session["sql_query_height"] = float(sql_query_height)
+        session[f"{page}_sql_query_height"] = float(sql_query_height)
         # 値がおかしかったらデフォルト値をセット
     except ValueError:
-        session["sql_query_height"] = DEFAULT_EDITOR_HEIGHT
+        session[f"{page}_sql_query_height"] = DEFAULT_EDITOR_HEIGHT
 
-def load_query_editor_height():
+def load_query_editor_height(page: str):
     # セッションの値を返す
-    return session.get("sql_query_height", DEFAULT_EDITOR_HEIGHT)
+    return session.get(f"{page}_sql_query_height", DEFAULT_EDITOR_HEIGHT)
 
-def set_scroll_to_editor(value=True):
+def set_scroll_to_editor(page: str, value: bool=True):
     """ エディタ部分へのスクロールフラグをセット
     """
-    session["scroll_to_editor"] = value
+    session[f"{page}_scroll_to_editor"] = value
 
-def pop_scroll_to_editor():
+def pop_scroll_to_editor(page: str):
     """ スクロールフラグを取り出してクリアする
     """
-    return session.pop("scroll_to_editor", False)
+    return session.pop(f"{page}_scroll_to_editor", False)
